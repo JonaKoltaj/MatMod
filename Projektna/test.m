521e7339b4dd9d04ce40494aa9bd5a4b9ef473ee
Jona Koltaj <jona.koltaj@gmail.com>
2024-08-20 17:07:32 +0200

    Funkcija za posevni met

--- Projektna/test.m
+++ Projektna/test.m
@@ -7,16 +7,18 @@ u0 = - 0.3;
 % X = diskrVeriznica(u0,Xstart,Xend,L,M);
 % risiDiskretno(X)
 
-xlevi = [0;0];
-xdesni = [1;3];
-sx = (xlevi(1)+xdesni(1))/2;
-sy = (xlevi(2)+xdesni(2))/2;
-li = norm(xdesni);
-v0 = [0.3;-1];
-v = odbojniKot(v0,li,xlevi,xdesni)
-figure
-hold on
-plot([xlevi(1),xdesni(1)],[xlevi(2),xdesni(2)])
-%ah whatever
-plot([sx,-v0(1)+sx],[-v0(2)+sy,sy])
-plot([sx,v(1)+sx],[sy,v(2)+sy])
+
+%funkcija odbojni kot deluje ce smo na desnih kvadrantih, zato kr sm
+%implicitno settala levga na [0;0] in gremo itak vedno od leve proti desni
+% desni = [1;-2];
+% sx = desni(1)/2;
+% sy = desni(2)/2;
+% li = norm(desni);
+% v0 = [0;-1];
+% v = odbojniKot(v0,li,[0;0],desni)
+% figure
+% hold on
+% plot([0,desni(1)],[0,desni(2)],'b')
+% plot([sx,-v0(1)+sx],[sy,-v0(2)+sy],'r')
+% plot([sx,v(1)+sx],[sy,v(2)+sy],'g')
+% axis equal
